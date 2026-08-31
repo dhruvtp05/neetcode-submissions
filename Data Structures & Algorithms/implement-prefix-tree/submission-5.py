494132fc43fc4854a717871ef7c12351c14502a2
Dhruv Patel <157432582+dhruvtp05@users.noreply.github.com>
2026-08-31 18:19:46 -0500

    Add: implement-prefix-tree - submission-5

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-5.py
@@ -0,0 +1,36 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.completed = False
+
+class PrefixTree:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+        curr.completed = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+
+        for char in word:
+            if char not in curr.children:
+                return False
+            curr = curr.children[char]
+        return curr.completed
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+
+        for char in prefix:
+            if char not in curr.children:
+                return False
+            curr = curr.children[char]
+        return True
+        
